f58ca80457f8c72bad3e812902273dd001d3b5ee
Peter Hickman <peterhickman386@googlemail.com>
2024-11-27 17:49:04 +0000

    refactor

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+make_link() {
+  echo "Create link ${APP}$1 for ${APP} $2"
+  ln -sf $BINARY/$APP $BINARY/${APP}$1
+}
+
 BINARY='/usr/local/bin'
 APP=snip
 
@@ -9,20 +14,11 @@ go build -ldflags="-s -w" $APP.go
 echo "Installing $APP to $BINARY"
 install $APP $BINARY
 
-echo "Create link ${APP}l for ${APP} --list"
-ln -sf $BINARY/$APP $BINARY/snipl
-
-echo "Create link ${APP}d for ${APP} --delete"
-ln -sf $BINARY/$APP $BINARY/snipd
-
-echo "Create link ${APP}i for ${APP} --import"
-ln -sf $BINARY/$APP $BINARY/snipi
-
-echo "Create link ${APP}e for ${APP} --export"
-ln -sf $BINARY/$APP $BINARY/snipe
-
-echo "Create link ${APP}s for ${APP} --search"
-ln -sf $BINARY/$APP $BINARY/snips
+make_link l --list
+make_link d --delete
+make_link i --import
+make_link e --export
+make_link s --search
 
 echo "Removing the build"
 rm $APP
